d67a5fd3fdc1688a4598845c4d096f4d92ae05cf
Rounit <rounitanand@gmail.com>
2021-10-08 02:32:59 +0530

    limited number of articles on a page

--- src/components/News.js
+++ src/components/News.js
@@ -2,23 +2,59 @@ import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 
 export class News extends Component {
-  
   constructor() {
     super();
     console.log("Hello I am a constructor from news component");
     this.state = {
       articles: [],
       loading: false,
+      page: 1,
     };
   }
 
   async componentDidMount() {
-      let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=4a7aa045cb104fa59e282441f781e76d";
+    let url =
+      "https://newsapi.org/v2/top-headlines?country=in&apiKey=4a7aa045cb104fa59e282441f781e76d&page=1&pageSize=20";
+    let data = await fetch(url);
+    let parsedData = await data.json();
+
+    this.setState({
+      articles: parsedData.articles,
+      totalResults: parsedData.totalResults,
+    });
+  }
+
+  handlePrevClick = async () => {
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=4a7aa045cb104fa59e282441f781e76d&page=${
+      this.state.page - 1
+    }&pageSize=20`;
+
+    let data = await fetch(url);
+    let parsedData = await data.json();
+
+    this.setState({
+      page: this.state.page - 1,
+      articles: parsedData.articles,
+    });
+  };
+
+  handleNextClick = async () => {
+    if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
+    } else {
+      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=4a7aa045cb104fa59e282441f781e76d&page=${
+        this.state.page + 1
+      }&pageSize=20`;
+
       let data = await fetch(url);
       let parsedData = await data.json();
-      
-      this.setState({articles : parsedData.articles});
-  }
+
+      this.setState({
+        page: this.state.page + 1,
+        articles: parsedData.articles,
+      });
+    }
+  };
+
   render() {
     return (
       <div className="container my-3">
@@ -29,14 +65,31 @@ export class News extends Component {
             return (
               <div className="col-md-4" key={element.url}>
                 <NewsItem
-                  title={element.title?element.title:''}
-                  description={element.description?element.description:''}
+                  title={element.title ? element.title : ""}
+                  description={element.description ? element.description : ""}
                   imageUrl={element.urlToImage}
                   newsUrl={element.url}
                 />
               </div>
             );
           })}
+        </div>
+        <div className="container d-flex justify-content-between my-3">
+          <button
+            disabled={this.state.page <= 1}
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handlePrevClick}
+          >
+            &larr; Previous
+          </button>
+          <button
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handleNextClick}
+          >
+            Next &rarr;
+          </button>
         </div>
       </div>
     );
